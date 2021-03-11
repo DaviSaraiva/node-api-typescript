7aa37d03b397e3ec8afa7037469a1c51c757ee55
DaviSaraiva <davi123467890@gmail.com>
2021-03-11 00:21:55 -0300

    confi do banco mongose, setup,  e docker

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { Server } from '@overnightjs/core';
 import { Application } from 'express';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   /*
@@ -20,6 +21,8 @@ export class SetupServer extends Server {
   public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
+    //iniciara aplicacao ja liga o banco
   }
 
   private setupExpress(): void {
@@ -31,8 +34,15 @@ export class SetupServer extends Server {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
   }
-
   public getApp(): Application {
     return this.app;
   }
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+  public async close(): Promise<void> {
+    await database.close();
+  }
+
+
 }
