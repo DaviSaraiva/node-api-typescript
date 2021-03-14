388b555a49663f8fac6c7437ffedc14b94d793a1
DaviSaraiva <davi123467890@gmail.com>
2021-03-14 16:58:53 -0300

    model beaches

---
+++ src/models/beach.ts
@@ -0,0 +1,43 @@
+import mongoose, { Document, Model } from "mongoose";
+
+export enum BeachPosition {
+    S = 'S',
+    E = 'E',
+    W = 'W',
+    N = 'N',
+}
+
+export interface Beach {
+    _id?: string;
+    name: string;
+    position: BeachPosition;
+    lat: number;
+    lng: number;
+}
+
+const schema = new mongoose.Schema(
+    {
+        lat: { type: Number, required: true },
+        lng: { type: Number, required: true },
+        name: { type: String, required: true },
+        position: { type: String, required: true },
+    },
+    {
+        //metodo padrao do mongosse, quando a gnt transforma  esse dado em json modificado, 
+        //ex: trasnforma o _id em id, deleta o_id padrão do mongose
+        //obs: não vou remover do banco somente da trasnfomação final
+        toJSON: {
+            transform: (_, ret): void => {
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.__v;
+            },
+        },
+    }
+);
+
+interface BeachModel extends Omit<Beach, '_id'>, Document { }
+export const Beach: Model<BeachModel> = mongoose.model('Beach', schema);
+
+//extou definindo um tipo do documents do mongoose mais o meu Beach, no caso meus campos.
+//depois criamos o model do mongoose. 
--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,8 @@
+import { Beach } from "@src/models/beach";
+
 describe('Beaches funcitional tests', () => {
+    //deletar todas as praias que tiver no banco, que o estado do teste estiver limpo quando a gente rodar
+    beforeAll(async () => await Beach.deleteMany({}));
     //criar prai vai ser uma operacao e vai ter um grupo especifico
     describe('When create beach', () => {
         it('Should create a beach with sucess', async () => {
@@ -13,5 +17,22 @@ describe('Beaches funcitional tests', () => {
             expect(response.status).toBe(201);
             expect(response.body).toEqual(expect.objectContaining(newBeach));
         });
+        // a entidade não pode ser processada,erro de validacao
+        it('should return 422 when there is a validation error', async () => {
+            const newBeach = {
+                lat: 'invalid_string',
+                lng: 151.289824,
+                name: 'Manly',
+                position: 'E',
+            };
+            const response = await global.testRequest.post('/beaches').send(newBeach);
+
+            expect(response.status).toBe(422);
+            expect(response.body).toEqual({
+                error:
+                    'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+            });
+        });
+
     });
 });
