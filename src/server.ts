aa425d2502621504e618667d56374112feac2663
DaviSaraiva <davi123467890@gmail.com>
2021-03-23 12:17:39 -0300

    retirando comentario

--- src/server.ts
+++ src/server.ts
@@ -8,18 +8,11 @@ import { BeachesController } from './controllers/beaches';
 import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
-  /*
-   * same as this.port = port, declaring as private here will
-   * add the port variable to the SetupServer instance
-   */
+
   constructor(private port = 3000) {
     super();
   }
 
-  /*
-   * We use a different method to init instead of using the constructor
-   * this way we allow the server to be used in tests and normal initialization
-   */
   public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
